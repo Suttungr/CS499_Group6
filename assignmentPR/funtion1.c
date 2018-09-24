780146473945331245e0a7793248c435634b533d
clawss <claudiacoronel55@gmail.com>
2018-09-24 11:59:30 -0700

    Update funtion1.c

--- assignmentPR/funtion1.c
+++ assignmentPR/funtion1.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
@@ -8,30 +7,23 @@ bool mantissa(char numString[], int *numerator, int *denominator);
 
 int main()
 {
-
-char number[] = "123.00456";
-int c, n, d;
-
-//if the conversion from C string to integers can take place
-if(characteristic(number, &c) && mantissa(number, &n, &d))
-{
-    //do some math with c, n, and d
-
-}
-else
-{
-    //handle the error on input
-
-}
+    char number[] = "123.00456";
+    int c, n, d;
+    characteristic(number, &c);
+    mantissa(number, &n, &d);
+    
+    printf("This is our 'c' (characteristic)number: %d\n", c);
+    printf("This is our 'n' (numerator) number: %d\n", n);
+    printf("This is our 'd' (denominator) number: %d\n", d);
 }
 
 bool characteristic(char numString[], int *c){
     char *copy = malloc(strlen(numString) + 1);
     strcpy(copy, numString);
     char *token = strtok(copy,".");
-
+    
     if(token){
-        c = atoi(token);
+        *c = atoi(token);
         return true;
     }
     else{
@@ -42,27 +34,22 @@ bool characteristic(char numString[], int *c){
 bool mantissa(char numString[], int *numerator, int *denominator){
     char *copy1 = malloc(strlen(numString) + 1);
     strcpy(copy1, numString);
-
-    char *token1 = strtok(copy1,".");
+    char *token1 = strchr(copy1,".");
     token1 = strtok(NULL, ".");
-
+    
+    char *token2 = token1;
     char temp[] = "1";
-
-    for(int i=0;i<strlen(token1)+1;i++)
+    int len = strlen(token2);
+    
+    for(int i=0;i<len;i++)
     {
-        //strcat(temp,"0");
+        strcat(&temp[i],"0");
     }
-
-    // the end result of this would be a way to get
-    // the denominator but i am not possitive
-
+    
     if(token1){
-      numerator = atoi(token1);
-      // do not know how to figure out the denominator part
-      // im guessing it has something to do with logs
-
-
-      return true;
+        *numerator = atoi(token1);
+        *denominator = atoi(temp);
+        return true;
     }
     else{
         return false;
